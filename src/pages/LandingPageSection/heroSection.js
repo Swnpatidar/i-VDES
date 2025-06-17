797b57f5b507861e96c4ab739b9451939510e21c
sawan patidar <sawan.patidar@gmail.com>
2025-06-17 14:59:27 +0530

    chnage hero section

--- src/pages/LandingPageSection/heroSection.js
+++ src/pages/LandingPageSection/heroSection.js
@@ -8,13 +8,12 @@ import { ROUTES } from '../../hooks/routes/routes-constant';
 // Amplify.configure(awsExport);
 
 const HeroSection = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
       {/* <Authenticator></Authenticator> */}
-    
-      <div className='hero-section'>
 
+      <div className='hero-section mb-4'>
         <div className='top-white-section'>
           <div className='row align-items-md-center'>
             <div className='col-6 col-md-6 col-sm-12'>
@@ -29,18 +28,22 @@ const HeroSection = () => {
           </div>
         </div>
         <div className='content-section py-4'>
-          <h1 className='text-lg'>Secure Every Image.</h1>
-          <h1 className='text-lg'>Safeguard Every Insight.</h1>
+          <div className='mb-3'>
+            <h1 className='text-lg'>Secure Every Image.</h1>
+            <h1 className='text-lg'>Safeguard Every Insight.</h1>
+          </div>
           <p className='para'>In the real of creativity, technology has become a powerful ally for artists, writers, designers, and creators of all kinds. Artficially intelligent (AI) tools have emarged as valuable companions, assisting naturally creative humans in their creative processes. </p>
         </div>
         {/* <div className='img-section'> */}
         <div className='row'>
-          <div className='col-7'>
+          <div className='col-12 col-md-7 col-sm-12 mb-3'>
             <img src={LANDING_IMAGE_1} alt="image" width="100%" height="100%" />
           </div>
-          <div className='col-5'>
-            <img src={LANDING_IMAGE_2} alt="image" width="100%" height="65%" className='mb-2' />
-            <Button label="Try Now" icon={ARROW_ICON} className="tryNowButton" />
+          <div className='col-12 col-md-5 col-sm-12 mb-3'>
+            <div className='imageBox mb-3'>
+              <img src={LANDING_IMAGE_2} alt="image" width="100%" height="100%" className='mb-2' />
+            </div>
+            <Button label="Try Now" icon={ARROW_ICON} className="tryNowButton" iconPosition='back' />
           </div>
         </div>
       </div>
